721c59afaebc7a4f46a802a7cdaecd6cb38c04b1
poommu <suphanut.pha@axonstech.com>
2023-05-03 09:24:31 +0700

    update init project

--- samantra/services/rm2_services/phantha/phantha.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import type { PlantQuantity } from "../../../common/common";
-
-export const protobufPackage = "samantra.services.rm2.phantha";
-
-export interface Phantha {
-  lotNo: string;
-  Quantities: PlantQuantity[];
-}
--- samantra/services/rm2_services/processing/processing.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import type { PlantQuantity } from "../../../common/common";
-
-export const protobufPackage = "samantra.services.rm2.processing";
-
-export interface Processing {
-  lotNo: string;
-  Quantities: PlantQuantity[];
-}
